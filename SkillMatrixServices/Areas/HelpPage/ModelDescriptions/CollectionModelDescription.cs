523b3d07f66f9f891214834ccaa9eceb2fc3cd87
Sravan Kumar <sravandnet@gmail.com>
2017-04-25 22:10:23 +0530

    Initial Commit

---
+++ SkillMatrixServices/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace SkillMatrixServices.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ SkillMatrixServices/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SkillMatrixServices.WebApiApplication" Language="C#" %>
